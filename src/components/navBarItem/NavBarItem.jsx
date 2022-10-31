194c047d2b34da64c3b73c02032158a9e3a83d4b
Elizaveta <lurovaj@gmail.com>
2022-10-31 12:14:25 +0300

    link

--- src/components/navBarItem/NavBarItem.jsx
+++ src/components/navBarItem/NavBarItem.jsx
@@ -5,7 +5,7 @@ import "./NavBarItem.scss";
 const NavBarItem = ({title}) => {
 
     return (
-        <Link to={`/genre/${title}`} target="_blank">
+        <Link to={`/library-app/build/genre/${title}`} target="_blank">
             <div className="genIcon">
                 <img src="/library-app/build/icons/icons8-open-book-50.png" alt="" />
             </div>
