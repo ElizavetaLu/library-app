31cbfa8d980cb64f7c69f31d56f7fbf00ce889eb
Elizaveta <lurovaj@gmail.com>
2023-03-30 20:02:53 +0300

    dec

--- src/components/bookCard/BookCard.jsx
+++ src/components/bookCard/BookCard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "../button/Button";
-import "./BookCard.scss"
+import "./BookCard.scss";
 
 
 const BookCard = ({ name, rating, cover, book_id, shelfItems, onAdd }) => {
